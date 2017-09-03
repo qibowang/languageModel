538eeed30c376cbc1e84043956ece02c587fab13
qibowang <qibowang@hotmail.com>
2017-09-03 16:14:15 +0800

    第三次提交：修改了多个ngram可能含有相同后缀而未分别处理的bug

--- src/main/java/com/lm/katz/bow/SuffixJoinProb.java
+++ src/main/java/com/lm/katz/bow/SuffixJoinProb.java
@@ -1,9 +1,13 @@
 package com.lm.katz.bow;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableUtils;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by root on 2017/9/1.
@@ -16,18 +20,23 @@ public class SuffixJoinProb extends Reducer<Text,Text,Text,Text>{
 	String prob="";
 	@Override
 	protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+		Configuration conf = context.getConfiguration();
+		List<Text> joinValues = new ArrayList<Text>();
 		for(Text value:values){
 			String temp[]=value.toString().split(" ");
 			if(temp[0].equals("suffix")){
-				ngram=temp[1];
-				prob=temp[2];
+				joinValues.add(WritableUtils.clone(value,conf));
 			}else if(temp[0].equals("prob")){
 				suffixProb=temp[1];
 			}
 		}
-		if (ngram.length()!=0){
+		for(Text value:joinValues){
+			String temp[]=value.toString().split(" ");
+			ngram=temp[0];
+			prob=temp[1];
 			resKey.set(ngram);
 			resValue.set((prob+"\t"+suffixProb).trim());
+			context.write(resKey,resValue);
 		}
 	}
 }
