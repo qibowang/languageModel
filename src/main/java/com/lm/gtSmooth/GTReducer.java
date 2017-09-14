153c6a72489d39d180ad97f55de520235d851af6
qibowang <qibowang@hotmail.com>
2017-09-14 16:28:52 +0800

    第五次提交：修改了潜在的若干错误

--- src/main/java/com/lm/gtSmooth/GTReducer.java
+++ src/main/java/com/lm/gtSmooth/GTReducer.java
@@ -2,6 +2,7 @@ package com.lm.gtSmooth;
 
 import org.apache.hadoop.conf.Configuration;
 
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableUtils;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -14,40 +15,54 @@ import java.util.List;
 /**
  * Created by root on 2017/5/24.
  */
-public class GTReducer extends Reducer<Text,Text,Text,Text>{
+public class GTReducer extends Reducer<Text,Text,Text,DoubleWritable>{
 	private Text resKey = new Text();
-	private Text resValue = new Text();
-	private Configuration conf =null;
+	private DoubleWritable resValue = new DoubleWritable();
+	private Configuration conf = null;
+	private int lrLabel=0;
+	@Override
+	protected void setup(Context context)
+			throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		Configuration conf = context.getConfiguration();
+		lrLabel=conf.getInt("lrLabel", 0);
+
+	}
 	@Override
 	protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-		long rawCountSum=0l;
-		conf=context.getConfiguration();
+		long rawCountSum = 0l;
+		conf = context.getConfiguration();
 		List<Text> list = new ArrayList<Text>();
 
-		for(Text joinValue :values){
-			String str=joinValue.toString();
-			//ngram\tgtcount\trawcount
-			String[] items=str.split("\t");
-			rawCountSum+=Long.parseLong(items[2]);
-			list.add(WritableUtils.clone(joinValue,conf));
+		for (Text joinValue : values) {
+			String str = joinValue.toString();
+			// ngram\tgtcount\trawcount
+			String[] items = str.split("\t");
+			rawCountSum += Long.parseLong(items[2]);
+			list.add(WritableUtils.clone(joinValue, conf));
 		}
-		for(Text joinValue:list){
-			String str=joinValue.toString();
-			//ngram\tgtcount\trawcount
-			String[] items=str.split("\t");
-			//ngram\tgtcount\trawcount
-			double prob=Double.parseDouble(items[1])/rawCountSum;
-
+		for (Text joinValue : list) {
+			String str = joinValue.toString();
+			// ngram\tgtcount\trawcount
+			String[] items = str.split("\t");
+			// ngram\tgtcount\trawcount
+			double prob = Double.parseDouble(items[1]) / rawCountSum;
 			if (prob > 1) {
 				prob = 1.0;
 			}else{
 				BigDecimal b = new BigDecimal(prob);
 				prob = b.setScale(7, BigDecimal.ROUND_HALF_UP).doubleValue();
 			}
+			if(lrLabel==2){
+				resKey.set(new StringBuilder(items[0]).reverse().toString());
+				resValue.set(prob);
+				context.write(resKey, resValue);
+			}else{
+				resKey.set(items[0]);
+				resValue.set(prob);
+				context.write(resKey, resValue);
+			}
 
-			resKey.set(items[0]);
-			resValue.set(String.valueOf(prob));
-			context.write(resKey,resValue);
 		}
 
 	}
